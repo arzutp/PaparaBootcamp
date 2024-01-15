7565c1925454340694c0320278bb841dcf386a79
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-15 14:43:35 +0300

    linq example 2

--- PaparaBootcamp.Homework1/Program.cs
+++ PaparaBootcamp.Homework1/Program.cs
@@ -10,17 +10,25 @@ namespace PaparaBootcamp.Homework1
             var products = Products();
             var customers = Customers();
 
-            //FromExample(products);
-            //WhereExample(products);
-            //OrderByExample(products);
-            //OrderByWithDescendingExample(products);
-            //GroupByExample(products);
-            //JoinExample(products, customers);
-
-
+            FromExample(products);
+            WhereExample(products);
+            OrderByExample(products);
+            OrderByWithDescendingExample(products);
+            GroupByExample(products);
+            JoinExample(products, customers);
+            DistinctExample(products);
+            AnyExample(products);
+            WhereAndJoinExample(products, customers);
+            AllExample(products);
+            TakeExample(products);
+            TopThreeHighValues(products);
+            TopHighStock(products);
         }
 
+
         
+
+
 
 
         #region from ve select
@@ -111,13 +119,117 @@ namespace PaparaBootcamp.Homework1
                                   ProductName = product.Name,
                                   ProductPrice = product.Price
                               };
-
+            Console.WriteLine("Join example: ");
             foreach (var data in joinExample)
             {
                 Console.WriteLine($"Customer: {data.CustomerName}, Product: {data.ProductName}, Price: {data.ProductPrice}");
             }
+            Console.WriteLine("---------------------------------------------------");
         }
         #endregion
+
+        #region Where and Join
+        //ürünler içinde Customer adı Arzu olan Ürün var mıdır?
+        private static void WhereAndJoinExample(List<Product> products, List<Customer> customers)
+        {           
+            Console.WriteLine("Join ve where example: ");
+            var joinExample = from customer in customers
+                              join product in products on customer.Id equals product.CustomerId
+                              where customer.Name == "Arzu"
+                              select new
+                              {
+                                  CustomerId = customer.Id,
+                                  CustomerName = customer.Name,
+                                  ProductName = product.Name,
+                                  ProductPrice = product.Price
+
+                              };
+
+            foreach (var item in joinExample)
+            {
+                Console.WriteLine($"Customer Name :  {item.CustomerName}, Product Name : {item.ProductName}");
+            }
+            Console.WriteLine("---------------------------------------------------");
+        }
+        #endregion
+
+        #region Distinct
+        //aynı olan değerleri almaz bende bu örnekte farklı fiyatları buldurmaya çalıştım
+        private static void DistinctExample(List<Product> products)
+        {
+            var distinctExample = (from product in products
+                                   select product.Price).Distinct();
+            Console.WriteLine("distinct example: ");
+            foreach (var item in distinctExample)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------------------------------------------------");
+        }
+        #endregion
+
+        #region Any Example
+        private static void AnyExample(List<Product> products)
+        {
+            //any var mı diye bir kontrol işlemidir ve bool değer döndürür.
+            bool anyProductExample = products.Any(product => product.CustomerId == 2);
+            Console.WriteLine("Ürünler içinde customer idsi 2 olan ürün var mıdır : " + anyProductExample);
+        }
+        #endregion
+
+        #region All Example
+        //All sorgusu da bool döner. Tabloda ki tüm değerleri kontrol eder ve hepsi aynı sonucu döndürüyorsa true değerini verir
+        private static void AllExample(List<Product> products)
+        {
+            Console.WriteLine("All Example : ");
+            bool allExample = products.All(item => item.Price > 0);
+            Console.WriteLine(allExample);
+            Console.WriteLine("---------------------------------------------------");
+        }
+        #endregion
+
+        #region Take Example
+        //take belli sayıda eleman döndürmek için kullanılır mesela ilk 3 değer gibi
+        private static void TakeExample(List<Product> products)
+        {
+            var takeExample = products.Take(3);
+            Console.WriteLine("Take example: ");
+            foreach (var product in takeExample)
+            {
+                Console.WriteLine($"Product Id: {product.Id}, Product Name: {product.Name}");
+            }
+            Console.WriteLine("---------------------------------------------------");
+        }
+        #endregion
+
+        #region OrderBy and Take Example
+        private static void TopThreeHighValues(List<Product> products)
+        {
+            Console.WriteLine("Order By and Take Example : ");
+            var topThreeHighValueProducts = (from product in products
+                                             orderby product.Price descending
+                                             select product).Take(3);
+            foreach (var product in topThreeHighValueProducts)
+            {
+                Console.WriteLine($"Product Name : {product.Name}, Product Price : {product.Price}");
+            }
+            Console.WriteLine("---------------------------------------------------");
+        }
+        #endregion
+
+        #region First Example
+        //First Example
+        //bu sorguda stokta en çok olan ürünü bulmaya çalıştım
+        private static void TopHighStock(List<Product> products)
+        {
+            var TopHighStock = (from product in products
+                                orderby product.Stock descending
+                                select product).FirstOrDefault();
+            Console.WriteLine($"Product Name : {TopHighStock!.Name}, Product Stock : {TopHighStock!.Stock}");
+        }
+        #endregion
+
+
 
 
 
