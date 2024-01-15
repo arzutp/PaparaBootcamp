35b7db3cf1f66e006d4128b9fba239d2e67177cc
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-15 16:42:55 +0300

    homework2 init

---
+++ PaparaBootcamp.Homework2/Models/Users/Entity/User.cs
@@ -0,0 +1,13 @@
+﻿namespace PaparaBootcamp.Homework2.Models.Users.Entity
+
+{
+    public class User
+    {
+        //User ( Id,Name,Surname,Age vb..) Entity
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+    }
+}
