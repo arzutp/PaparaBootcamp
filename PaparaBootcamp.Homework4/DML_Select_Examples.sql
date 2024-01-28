4882dcc4abfe235572843295caa6b2def92c95c3
arzutepe <38106558+arzutp@users.noreply.github.com>
2024-01-28 23:43:32 +0300

    right-left_join

--- PaparaBootcamp.Homework4/DML_Select_Examples.sql
+++ PaparaBootcamp.Homework4/DML_Select_Examples.sql
@@ -9,18 +9,19 @@ select *  from books where authorId=3
 --bu şekilde tablo ismini belirleyebiliriz
 select bookname 'Book Name' from books where authorId=2
 
-
---iki tabloyu birleştirme
-select bookname 'Book Name', name 'Author Name'
-From Books 
-join Author on Books.authorId = Author.id
-
-
 select * from Orders where userId=1
 
 
 select * from OrderItems
 where orderId = 1;
+
+
+--join examples
+--iki tabloyu birleştirme
+select bookname 'Book Name', name 'Author Name'
+From Books 
+join Author on Books.authorId = Author.id
+
 
 
 --3 tane tabloyu birleştirme
@@ -42,3 +43,18 @@ Join Orders o on o.userId=u.id
 join OrderItems oitems on o.id=oitems.orderId
 join Books b on b.id=oitems.bookId
 join Author a on b.authorId=a.id
+
+
+--left join
+--otomatik olarakta left join yapar zaten 
+select bookname as 'Book Name', name as 'Author Name'
+From Books 
+left join Author on Books.authorId = Author.id
+
+
+--right join
+--books tablosunda karşılığı olmayan authors ları da al demek oluyor
+select bookname as 'Book Name', name as 'Author Name'
+From Books 
+right join Author on Books.authorId = Author.id
+
